e786e95d9c88413addf66c5b0ef178f079fd9787
Jackson Schuetzle <49105682+Jschuetzle@users.noreply.github.com>
2022-02-27 19:17:27 -0600

    Update TurnPIDController.java
    added final comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareFunctionality/TurnPIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareFunctionality/TurnPIDController.java
@@ -3,7 +3,16 @@ package org.firstinspires.ftc.teamcode.HardwareFunctionality;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class TurnPIDController {
-
+    
+    /*
+        If you're going to use a custom PID controller for a mechanism other than the wheels
+        (RoadRunner does that one for you), I'd recommend you get an idea what the custom
+        PID code might look like here. Don't just use this code...it's only a TURN pid!
+        I got this code from a youtube video, so you might have to do some fishing to find
+        it. But, you probably should make a PID controller for any mechanism that has a possibility
+        for error.
+    */
+    
     private double targetAngle;
     private double Kp, Ki, Kd;
     private double integralSum, lastError, lastTime;
