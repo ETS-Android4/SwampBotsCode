70c9d92a409c6acdf483e237f3e31d77de5e1369
Jackson Schuetzle <49105682+Jschuetzle@users.noreply.github.com>
2022-02-27 19:04:53 -0600

    Update Webcam.java
    Camera class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/Webcam.java
@@ -1,36 +1,27 @@
 package org.firstinspires.ftc.teamcode.Camera;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.Hardware;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 
 public class Webcam {
     public OpenCvWebcam camera = null;
 
-
+    //We need a constructor, but we're not doing any initializing for custom
+    //variables, so leave empty
     public Webcam(){
 
     }
-
-
+    
     public void init(HardwareMap hw){
 
         //Camera name and id stuff that needs to be set up in order to initialize the camera object
         int cameraMonitorViewId = hw.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hw.appContext.getPackageName());
 
+        //Get the name from the configuration
         WebcamName webcamName = hw.get(WebcamName.class, "Webcam 1");
 
 
